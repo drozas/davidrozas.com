aa9f074d8ff14c8eaf205d8f3bf2668fda94737b
David Rozas <david.rozas@gmail.com>
2015-01-03 17:44:25 +0100

    Updating cv

--- cv.php
+++ cv.php
@@ -5,36 +5,14 @@
 	
 	check_language($_GET['set_language']);
 
-	/*$form= "<FORM METHOD=POST ACTION=\"process_reference_answer.php\">
-			<br \><label for=\"name\">Format</label>
-			<select size=\"1\" name=\"format\">
-            <option value=\"pdf\" selected>PDF</option>
-            <option value=\"odt\">ODT</option>
-            <option value=\"doc\">DOC</option>
-            </select>
-			<br \><label for=\"name\">Language</label>
-			<select size=\"1\" name=\"lang\">
-            <option value=\"en\" selected>English</option>
-            <option value=\"es\">Spanish</option>
-            </select>
-			<br \><label for=\"name\">Name</label> <input type=\"text\" id=\"name\" />
-            <br \><label for=\"name\">Name</label> <input type=\"text\" id=\"name\" />
-            <br \>Comment:
-            <br \><TEXTAREA NAME=\"recommendationLetter\"COLS=40 ROWS=6></TEXTAREA>
-
-           	<br \><INPUT TYPE=\"submit\"name=\"submit\" value=\"Send\">
-            </FORM>";*/
-            $text = "If you need my CV or any reference letter, please send me an e-mail to <a href=\"mailto:david.rozas@gmail.com\">david.rozas@gmail.com</a> 
-								<br /> You can indicate your preferred document format (.pdf, .odt, .doc, ...) and language (English or Spanish). You can also contact me
-								through <a href=\"http://uk.linkedin.com/in/drozas\" target=\"_blank\">LinkedIn</a>." ;
-//drozas: This will be in a db or at least a file
+            $text = "If you need my CV or any reference letter, please <a href=\"mailto:david.rozas@gmail.com\">send me an e-mail</a>, indicating your preferred document format (.pdf, .odt, .doc, ...) and language (English or Spanish). You can also contact me
+								through <a href=\"http://uk.linkedin.com/in/drozas\" target=\"_blank\">LinkedIn</a>, where I keep an updated version of my CV." ;
 	$items = array(		array(
 								"Request CV", 
 								$text,
 								"Solicitar CV",
-								"Si quieres solictar mi CV o cartas de referencia, puedes enviarme un correo a <a href=\"mailto:david.rozas@gmail.com\">david.rozas@gmail.com</a> 
-								<br /> Puedes indicar el formato (.pdf, .odt, .doc, ...) y el idioma (inglés o español) en el que prefieres recibirlo. Puedes contactarme igualmente
-								a través de <a href=\"http://uk.linkedin.com/in/drozas\" target=\"_blank\">LinkedIn</a>.")
+								"Si quieres solictar mi CV o cartas de referencia, puedes <a href=\"mailto:david.rozas@gmail.com\">enviarme un correo</a>, indicando el formato (.pdf, .odt, .doc, ...) y el idioma (inglés o español) en el que prefieres recibirlo. Puedes contactarme igualmente
+								a través de <a href=\"http://uk.linkedin.com/in/drozas\" target=\"_blank\">LinkedIn</a>, en donde mantengo una versión actualizada de mi CV.")
 					);
 
 	print_header($_SESSION["lang"]);
